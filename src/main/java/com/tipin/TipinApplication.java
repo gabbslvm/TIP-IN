f543fca9507464a084c4c11f37c8088150b71bbf
gabbslvm <jgsalvame@gmail.com>
2026-08-30 20:58:52 +0800

    Initial Spring Boot Project Setup for TIP-IN

---
+++ src/main/java/com/tipin/TipinApplication.java
@@ -0,0 +1,13 @@
+package com.tipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tipin/TipinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
