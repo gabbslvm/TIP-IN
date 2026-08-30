5b6275a7c0773897cfc3270e58dd320b4b62c2df
gabbslvm <jgsalvame@gmail.com>
2026-08-30 23:29:11 +0800

    update

--- src/main/java/com/tipin/Main.java
+++ src/main/java/com/tipin/Main.java
@@ -1,19 +1,4 @@
-/* package com.tipin;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Main {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
-
-}
-*/
-
-/* package com.tipin;
+package com.tipin;
 
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
@@ -21,10 +6,8 @@ import java.net.InetSocketAddress;
 public class Main {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/api/users", new com.tipin.handler.UserHandler());
         server.setExecutor(null);
         server.start();
         System.out.println("Server running on port 8080");
     }
 }
-*/
