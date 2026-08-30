bda44bf769a0c34794228840993cd0b2252f51ab
gabbslvm <jgsalvame@gmail.com>
2026-08-30 23:30:55 +0800

    update

--- src/main/java/com/tipin/Main.java
+++ src/main/java/com/tipin/Main.java
@@ -1,4 +1,4 @@
-/*package com.tipin;
+package com.tipin;
 
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
@@ -6,10 +6,8 @@ import java.net.InetSocketAddress;
 public class Main {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/api/users", new com.tipin.handler.UserHandler());
         server.setExecutor(null);
         server.start();
         System.out.println("Server running on port 8080");
     }
 }
-*/
